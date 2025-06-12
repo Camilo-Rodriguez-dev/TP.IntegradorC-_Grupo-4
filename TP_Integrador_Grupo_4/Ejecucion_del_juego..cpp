ddc8ae23753942101ed665ee978f2ad1948b3865
Camilo Agustin Rodriguez Maradey <camilo00.rm@gmail.com>
2025-06-11 23:42:18 -0300

    Add files via upload

---
+++ TP_Integrador_Grupo_4/Ejecucion_del_juego..cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "FuncionesTrey.h"
+
+using namespace std;
+
+juego(nombre1, nombre2, carta1, carta2, PTJugador1, PTJugador2, contadorBugsJ1, contadorBugsJ2, puntosAcumuladosJ1, puntosAcumuladosJ2, ceroBugsJ1, ceroBugsJ2, acum20J1, acum20J2, rankingMejores5Jugadores, rankingMejores5Puntajes, nombreUltimoGanador, ultimoPuntaje);
+{
+
+}
